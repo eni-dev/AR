5d299399f854680b4d07f5e7772d425f79874c78
eni-dev <mimoun.fellah@voila.fr>
2014-05-15 16:15:17 +0200

    maj

--- js/map.js
+++ js/map.js
@@ -1,20 +1,33 @@
-document.addEventListener("deviceready", onDeviceReady, false);
-
-function onDeviceReady() {
-getCurrentLocation();
-downloadTile();}
-
 function getCurrentLocation() {
-			window.requestFileSystem(LocalFileSystem.PERSISTENT, 0,	filesys,
-				onError);
-		}
-function filesys(FS)  {
+	/*$("#debug").append("Getting current location ...<br />"); 
+	navigator.geolocation.getCurrentPosition(
+		function(position) {
+			$("#debug").append("Current location : " + position.coords.latitude + ", " + position.coords.longitude + "<br />");
+			window.requestFileSystem(LocalFileSystem.PERSISTENT, 0,
+				function(FS) {
 					$("#debug").append("Root path : " + FS.root.fullPath + "<br />");
-					downloadMap(48.844077, 2.3737547, 10, FS);
-				}
-function onError(error) {
+					downloadMap(position.coords.latitude, position.coords.longitude, 10, FS);
+				},
+				function(error) {
 					$("#debug").append("Can't get filesystem<br />");
 				}
+			);
+		},
+		function(error) {
+			$("#debug").append("Can't get current location<br />");*/
+			window.requestFileSystem(LocalFileSystem.PERSISTENT, 0,
+				function(FS) {
+					$("#debug").append("Root path : " + FS.root.fullPath + "<br />");
+					downloadMap(48.844077, 2.3737547, 10, FS);
+				},
+				function(error) {
+					$("#debug").append("Can't get filesystem<br />");
+				}
+			);
+		/*},
+		{ enableHighAccuracy: true }
+	);*/
+}
 
 function downloadMap(lat, lon, rayon, fileSystem) {
 	var xtile = long2tile(lon, 15);
@@ -24,6 +37,22 @@ function downloadMap(lat, lon, rayon, fileSystem) {
 	var y2tile = lat2tile(lat-rayon*0.00899928005759539236861051115911, 15);
 	
 	$("#debug").append("Starting map download ...<br />");
+	
+	/*var tile = 1;
+	//var fileTransfer = new FileTransfer();
+	var x = 0;
+	var y = 0;
+	$("#debug").append("xtile = " + xtile + "; x2tile = " + x2tile + "<br />");
+	$("#debug").append("ytile = " + ytile + "; y2tile = " + y2tile + "<br />");
+	for(x = x2tile; x <= 2*xtile-x2tile; x++) {
+		for(y = 2*ytile-y2tile; y <= y2tile; y++) {
+			var i = x;
+			var j = y;
+			$("#debug").append("i = " + i + "; j = " + j + "<br />");
+			setTimeout(downloadTile(i, j, fileSystem), tile*500);
+			tile++;
+		}
+	}*/
 	minTileX = x2tile;
 	minTileY = 2*ytile-y2tile;
 	maxTileX = 2*xtile-x2tile;
@@ -34,9 +63,9 @@ function downloadMap(lat, lon, rayon, fileSystem) {
 function downloadTile(tileX, tileY, minTileX, minTileY, maxTileX, maxTileY, fileSystem) {
 	var fileTransfer = new FileTransfer();
 	fileTransfer.download(
-		'http://a.tile.openstreetmap.org/15/' + tileX + '/' + tileY + '.png',
+		encodeURI("http://a.tile.openstreetmap.org/15/" + tileX + "/" + tileY + ".png"),
 		//fileSystem.root.fullPath+"sdcard/openstreetmap/15/" + tileX + "/" + tileY + ".png",
-		'file:///sdcard/openstreetmap/15/' + tileX + '/' + tileY + '.png',
+		"/sdcard/openstreetmap/15/" + tileX + "/" + tileY + ".png",
 		function(entry) {
 			$("#debug").append("Downloaded tile " + tileX + ":" + tileY + " from server a<br />");
 			if(tileX < maxTileX) {
@@ -56,9 +85,9 @@ function downloadTile(tileX, tileY, minTileX, minTileY, maxTileX, maxTileY, file
 		},
 		function(error) {
 			setTimeout(fileTransfer.download(
-				"http://b.tile.openstreetmap.org/15/" + tileX + "/" + tileY + ".png",
+				encodeURI("http://b.tile.openstreetmap.org/15/" + tileX + "/" + tileY + ".png"),
 				//fileSystem.root.fullPath+"sdcard/openstreetmap/15/" + tileX + "/" + tileY + ".png",
-				"file:///sdcard/openstreetmap/15/" + tileX + "/" + tileY + ".png",
+				"/sdcard/openstreetmap/15/" + tileX + "/" + tileY + ".png",
 				function(entry) {
 					$("#debug").append("Downloaded tile " + tileX + ":" + tileY + " from server b<br />");
 					if(tileX < maxTileX) {
@@ -78,9 +107,9 @@ function downloadTile(tileX, tileY, minTileX, minTileY, maxTileX, maxTileY, file
 				},
 				function(error) {
 					setTimeout(fileTransfer.download(
-						"http://c.tile.openstreetmap.org/15/" + tileX + "/" + tileY + ".png",
+						encodeURI("http://c.tile.openstreetmap.org/15/" + tileX + "/" + tileY + ".png"),
 						//fileSystem.root.fullPath+"sdcard/openstreetmap/15/" + tileX + "/" + tileY + ".png",
-						"file:///sdcard/openstreetmap/15/" + tileX + "/" + tileY + ".png",
+						"/sdcard/openstreetmap/15/" + tileX + "/" + tileY + ".png",
 						function(entry) {
 							$("#debug").append("Downloaded tile " + tileX + ":" + tileY + " from server c<br />");
 							if(tileX < maxTileX) {
@@ -130,13 +159,65 @@ function downloadTile(tileX, tileY, minTileX, minTileY, maxTileX, maxTileY, file
 									$("#debug").append("Map download finished ...<br />");
 								}
 							}
+						},
+						true
+					), 500);
+				},
+				true
+			), 500);
+		},
+		true
+	);
+}
+
+/*function downloadTile(tileX, tileY, fileSystem) {
+	var fileTransfer = new FileTransfer();
+	fileTransfer.download(
+		"http://a.tile.openstreetmap.org/15/" + tileX + "/" + tileY + ".png",
+		fileSystem.root.fullPath+"sdcard/openstreetmap/15/" + tileX + "/" + tileY + ".png",
+		function(entry) {
+			$("#debug").append("Downloaded tile " + tileX + ":" + tileY + " from server a<br />");
+		},
+		function(error) {
+			setTimeout(fileTransfer.download(
+				"http://b.tile.openstreetmap.org/15/" + tileX + "/" + tileY + ".png",
+				fileSystem.root.fullPath+"sdcard/openstreetmap/15/" + tileX + "/" + tileY + ".png",
+				function(entry) {
+					$("#debug").append("Downloaded tile " + tileX + ":" + tileY + " from server b<br />");
+				},
+				function(error) {
+					setTimeout(fileTransfer.download(
+						"http://c.tile.openstreetmap.org/15/" + tileX + "/" + tileY + ".png",
+						fileSystem.root.fullPath+"sdcard/openstreetmap/15/" + tileX + "/" + tileY + ".png",
+						function(entry) {
+							$("#debug").append("Downloaded tile " + tileX + ":" + tileY + " from server c<br />");
+						},
+						function(error) {
+							$("#debug").append("Can't get tile " + tileX + ":" + tileY + "<br />");
+							$("#debug").append("Error :");
+							if(error.code==FileTransferError.FILE_NOT_FOUND_ERR) {
+								$("#debug").append("&nbsp;&nbsp;&nbsp;&nbsp;Code = FILE NOT FOUND<br />");
+							}
+							else if(error.code==FileTransferError.INVALID_URL_ERR) {
+								$("#debug").append("&nbsp;&nbsp;&nbsp;&nbsp;Code = INVALID URL<br />");
+							}
+							else if(error.code==FileTransferError.CONNECTION_ERR) {
+								$("#debug").append("&nbsp;&nbsp;&nbsp;&nbsp;Code = CONNECTION<br />");
+							}
+							else if(error.code==FileTransferError.ABORT_ERR) {
+								$("#debug").append("&nbsp;&nbsp;&nbsp;&nbsp;Code = ABORT<br />");
+							}
+							$("#debug").append("&nbsp;&nbsp;&nbsp;&nbsp;Source = " + error.source + "<br />");
+							$("#debug").append("&nbsp;&nbsp;&nbsp;&nbsp;Target = " + error.target + "<br />");
+							$("#debug").append("&nbsp;&nbsp;&nbsp;&nbsp;HTTP_status = " + error.http_status + "<br />");
 						}
 					), 500);
 				}
 			), 500);
 		}
 	);
-}
+}*/
+
 function long2tile(lon,zoom) {
 	return (Math.floor((lon+180)/360*Math.pow(2,zoom)));
 }
